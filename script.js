295f6708f2ae91b89d602896909fcd324037c49f
Ryan Thorn <ryandthorn@gmail.com>
2019-06-13 09:29:57 -0700

    Fix how buttons display; lint w/ StandardJS

--- script.js
+++ script.js
@@ -1,152 +1,152 @@
-"use strict";
-/*** Contact buttons ***/
-$("#wrap__contacts").on("click", function(e) {
-  e.preventDefault();
+'use strict'
+/** * Contact buttons ***/
+$('#wrap__contacts').on('click', function (e) {
+  e.preventDefault()
   switch (e.target.id) {
-    case "email":
-      const email = "ryandthorn@gmail.com";
-      const subject = "Portfolio";
-      document.location = `mailto:${email}?subject=${subject}`;
-      break;
-    case "github":
-      const githubURL = "https://github.com/ryandthorn";
-      window.open(githubURL, "_blank");
-      break;
-    case "linkedin":
-      const linkedinURL = "https://www.linkedin.com/in/ryandthorn/";
-      window.open(linkedinURL, "_blank");
-      break;
+    case 'email':
+      const email = 'ryandthorn@gmail.com'
+      const subject = 'Portfolio'
+      document.location = `mailto:${email}?subject=${subject}`
+      break
+    case 'github':
+      const githubURL = 'https://github.com/ryandthorn'
+      window.open(githubURL, '_blank')
+      break
+    case 'linkedin':
+      const linkedinURL = 'https://www.linkedin.com/in/ryandthorn/'
+      window.open(linkedinURL, '_blank')
+      break
   }
-});
-/*** Navigation bar ***/
-$("#navigation").on("click", function(e) {
-  e.preventDefault();
+})
+/** * Navigation bar ***/
+$('#navigation').on('click', function (e) {
+  e.preventDefault()
   switch (e.target.id) {
-    case "nav-home":
-      if ($("#home").hasClass("hidden")) {
-        highlightNav("home");
-        displayArea("home");
+    case 'nav-home':
+      if ($('#home').hasClass('hidden')) {
+        highlightNav('home')
+        displayArea('home')
       }
-      break;
-    case "wrap-home":
-      if ($("#home").hasClass("hidden")) {
-        highlightNav("home");
-        displayArea("home");
+      break
+    case 'wrap-home':
+      if ($('#home').hasClass('hidden')) {
+        highlightNav('home')
+        displayArea('home')
       }
-      break;
-    case "nav-about":
-      if ($("#about").hasClass("hidden")) {
-        highlightNav("about");
-        displayArea("about");
+      break
+    case 'nav-about':
+      if ($('#about').hasClass('hidden')) {
+        highlightNav('about')
+        displayArea('about')
       }
-      break;
-    case "wrap-about":
-      if ($("#about").hasClass("hidden")) {
-        highlightNav("about");
-        displayArea("about");
+      break
+    case 'wrap-about':
+      if ($('#about').hasClass('hidden')) {
+        highlightNav('about')
+        displayArea('about')
       }
-      break;
-    case "nav-projects":
-      if ($("#projects").hasClass("hidden")) {
-        highlightNav("projects");
-        displayArea("projects");
+      break
+    case 'nav-projects':
+      if ($('#projects').hasClass('hidden')) {
+        highlightNav('projects')
+        displayArea('projects')
       }
-      break;
-    case "wrap-projects":
-      if ($("#projects").hasClass("hidden")) {
-        highlightNav("projects");
-        displayArea("projects");
+      break
+    case 'wrap-projects':
+      if ($('#projects').hasClass('hidden')) {
+        highlightNav('projects')
+        displayArea('projects')
       }
-      break;
+      break
   }
-});
+})
 // Navigation bar - keyboard
-$("#navigation").keypress(function(e) {
-  if (e.which == 13) {
-    $(`#${e.target.id}`).click();
+$('#navigation').keypress(function (e) {
+  if (e.which === 13) {
+    $(`#${e.target.id}`).click()
   }
-});
+})
 // Utilities
 const displayArea = area => {
-  $(".area").addClass("hidden");
-  $(`#${area}`).removeClass("hidden");
-};
+  $('.area').addClass('hidden')
+  $(`#${area}`).removeClass('hidden')
+}
 const highlightNav = area => {
-  $(".nav-link").removeClass("nav-active");
-  $(`#nav-${area}`).addClass("nav-active");
-};
-/*** Project navigation ***/
-$("#navigate-projects").click(function(e) {
-  e.preventDefault();
-  if (e.target.id === "previous") {
-    const currentProject = $(".current");
-    const previousProject = currentProject.prev();
+  $('.nav-link').removeClass('nav-active')
+  $(`#nav-${area}`).addClass('nav-active')
+}
+/** * Project navigation ***/
+$('#navigate-projects').click(function (e) {
+  e.preventDefault()
+  if (e.target.id === 'previous') {
+    const currentProject = $('.current')
+    const previousProject = currentProject.prev()
     if (previousProject.length === 0) {
-      const lastChild = $("#view-projects section").last();
-      currentProject.toggleClass("hidden current");
-      lastChild.toggleClass("hidden current");
+      const lastChild = $('#view-projects section').last()
+      currentProject.toggleClass('hidden current')
+      lastChild.toggleClass('hidden current')
     } else {
-      currentProject.toggleClass("hidden current");
-      previousProject.toggleClass("hidden current");
+      currentProject.toggleClass('hidden current')
+      previousProject.toggleClass('hidden current')
     }
-  } else if (e.target.id === "next") {
-    const currentProject = $(".current");
-    const nextProject = currentProject.next();
+  } else if (e.target.id === 'next') {
+    const currentProject = $('.current')
+    const nextProject = currentProject.next()
     if (nextProject.length === 0) {
-      const firstChild = $("#view-projects section").first();
-      currentProject.toggleClass("hidden current");
-      firstChild.toggleClass("hidden current");
+      const firstChild = $('#view-projects section').first()
+      currentProject.toggleClass('hidden current')
+      firstChild.toggleClass('hidden current')
     } else {
-      currentProject.toggleClass("hidden current");
-      nextProject.toggleClass("hidden current");
+      currentProject.toggleClass('hidden current')
+      nextProject.toggleClass('hidden current')
     }
   }
-});
-/*** Animation ***/
-$(function() {
-  function endAnimation(_node) {
-    function handleAnimationEnd() {
-      node.classList.remove("animated", _node.animation);
-      node.removeEventListener("animationend", handleAnimationEnd);
+})
+/** * Animation ***/
+$(function () {
+  function endAnimation (_node) {
+    function handleAnimationEnd () {
+      node.classList.remove('animated', _node.animation)
+      node.removeEventListener('animationend', handleAnimationEnd)
     }
-    const node = document.querySelector(_node.element);
-    node.addEventListener("animationend", handleAnimationEnd);
+    const node = document.querySelector(_node.element)
+    node.addEventListener('animationend', handleAnimationEnd)
   }
   const animatedElements = [
     {
-      element: "#navigation",
-      animation: "bounceInDown"
+      element: '#navigation',
+      animation: 'bounceInDown'
     },
     {
-      element: "#name",
-      animation: "fadeIn"
+      element: '#name',
+      animation: 'fadeIn'
     },
     {
-      element: "#hr__title",
-      animation: "fadeIn"
+      element: '#hr__title',
+      animation: 'fadeIn'
     },
     {
-      element: "#title1",
-      animation: "slideInLeft"
+      element: '#title1',
+      animation: 'slideInLeft'
     },
     {
-      element: "#title2",
-      animation: "slideInRight"
+      element: '#title2',
+      animation: 'slideInRight'
     },
     {
-      element: "#email",
-      animation: "fadeIn"
+      element: '#email',
+      animation: 'fadeIn'
     },
     {
-      element: "#github",
-      animation: "fadeIn"
+      element: '#github',
+      animation: 'fadeIn'
     },
     {
-      element: "#linkedin",
-      animation: "fadeIn"
+      element: '#linkedin',
+      animation: 'fadeIn'
     }
-  ];
+  ]
   for (let node of animatedElements) {
-    endAnimation(node);
+    endAnimation(node)
   }
-});
+})
